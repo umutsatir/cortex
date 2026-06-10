f0b25770d146f4251959662d89ca5f4f237d7655
umut.eth <u.satir2021@gtu.edu.tr>
2026-06-10 22:24:47 +0300

    fix panel toggles not working in today view

--- src/components/today/TodayFocus.tsx
+++ src/components/today/TodayFocus.tsx
@@ -1,13 +1,46 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useTaskStore } from '../../store/taskStore';
 import { TodayTaskList } from './TodayTaskList';
 import { EisenhowerMatrix } from '../eisenhower/EisenhowerMatrix';
 import { Timebox } from '../timebox/Timebox';
 
 export function TodayFocus() {
+  const showBrainDump = useTaskStore((s) => s.showBrainDump);
+  const showTimebox   = useTaskStore((s) => s.showTimebox);
+
   return (
     <>
-      <TodayTaskList />
+      <AnimatePresence initial={false}>
+        {showBrainDump && (
+          <motion.div
+            key="today-left"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: 280, opacity: 1 }}
+            exit={{ width: 0, opacity: 0 }}
+            transition={{ duration: 0.2, ease: 'easeInOut' }}
+            className="overflow-hidden flex-shrink-0 h-full"
+          >
+            <TodayTaskList />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <EisenhowerMatrix />
-      <Timebox />
+
+      <AnimatePresence initial={false}>
+        {showTimebox && (
+          <motion.div
+            key="today-right"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: 300, opacity: 1 }}
+            exit={{ width: 0, opacity: 0 }}
+            transition={{ duration: 0.2, ease: 'easeInOut' }}
+            className="overflow-hidden flex-shrink-0 h-full"
+          >
+            <Timebox />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
